0f38776f2501b34a561aed9202613c6b0780b33b
soufiane chaghil <soufianechaghil@gmail.com>
2022-07-07 19:05:56 +0100

    insertion sort is done

--- src/pages/insertionSort/index.js
+++ src/pages/insertionSort/index.js
@@ -2,52 +2,39 @@ import React, { useCallback, useEffect, useState } from "react";
 import "./style.css";
 
 function Index() {
-
-
   const [arrayToSort, setArrayToSort] = useState([]);
   const [, updateState] = useState();
   const forceUpdate = useCallback(() => updateState({}), []);
-  const [speed, setSpeed] = useState(1000)
-  const [length, setLength] = useState(4)
-  const [sortedArray, setSortedArray] = useState([])
-  const [sorting, setSorting] = useState(false)
+  const [speed, setSpeed] = useState(1000);
+  const [length, setLength] = useState(6);
+  const [sortedArray, setSortedArray] = useState([]);
+  const [sorting, setSorting] = useState(false);
+  const [logs, setLogs] = useState([]);
+  const [showLogs, setShlowLogs] = useState(false);
 
   const generateArray = (num) => {
+    const res = [];
 
-    const res = []
-
-    while(res.length < num){
-        const randomNumber =  Math.floor(Math.random() * 2 * num)
-        if( randomNumber !== 0 && res.find(x => x === randomNumber) === undefined ) res.push(randomNumber)
+    while (res.length < num) {
+      const randomNumber = Math.floor(Math.random() * 2 * num);
+      if (
+        randomNumber !== 0 &&
+        res.find((x) => x === randomNumber) === undefined
+      )
+        res.push(randomNumber);
     }
-    return res
-  }
+    return res;
+  };
 
-    const setTim = () => {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve()
-            }, speed);
-        })
-    }
+  const setTim = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, speed);
+    });
+  };
 
-    const sortFun = (arr) => {
-      for (let i = 0; i < arr.length - 1; i++) {
-        let j = i;
-  
-        while (arr[j] > arr[j + 1]) {
-          const tmp = arr[j + 1];
-          arr[j + 1] = arr[j];
-          arr[j] = tmp;
-          j = j - 1;
-  
-        }
-      }
-    }
-
-
-  const insertion_sort = async (arr) => {
-    setSorting(true)
+  const sortFun = (arr) => {
     for (let i = 0; i < arr.length - 1; i++) {
       let j = i;
 
@@ -56,83 +43,160 @@ function Index() {
         arr[j + 1] = arr[j];
         arr[j] = tmp;
         j = j - 1;
-
-        setArrayToSort(arr)
-        forceUpdate()
-
-        await setTim()
-
       }
     }
-    
-    setSorting(false)
+  };
 
+  const insertion_sort = async (arr) => {
+    const rr = [];
+    setSorting(true);
+    for (let i = 0; i < arr.length - 1; i++) {
+      let j = i;
+
+      while (arr[j] > arr[j + 1]) {
+        const innerLog = `The element ${arr[j]} is bigger than ${
+          arr[j + 1]
+        } even though it precedes it, so we are going to swap them`;
+        rr.push({ title: false, body: innerLog });
+
+        const tmp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = tmp;
+        j = j - 1;
+
+        const afterLog = `The array after the swap is: `;
+        rr.push({ title: false, body: afterLog, array: JSON.stringify(arr) });
+
+        setArrayToSort(arr);
+        forceUpdate();
+
+        await setTim();
+      }
+    }
+    setLogs(logs.concat(rr));
+    setSorting(false);
   };
 
   const sort = () => {
-    insertion_sort(arrayToSort)
+    insertion_sort(arrayToSort);
   };
 
+  console.log(logs);
 
   const returnBarForNum = (num, idx) => {
-
     // check if the item is in its natural place place
-    const isThisOneDone = sortedArray.indexOf(num) === arrayToSort.indexOf(num)
-    const width = (length === 10 || length === 4) ? 20 : length === 50 ? 15 : length === 100 ? 5 : 4
+    const isThisOneDone = sortedArray.indexOf(num) === arrayToSort.indexOf(num);
+    const width =
+      length === 10 || length === 6
+        ? 20
+        : length === 50
+        ? 15
+        : length === 100
+        ? 5
+        : 4;
 
-    const height = (length === 10 || length === 4) ? num * 25 : length === 50 ? num * 5 : length === 100 ? num * 3 : num * 2
-
+    const height =
+      length === 10 || length === 6
+        ? num * 25
+        : length === 50
+        ? num * 5
+        : length === 100
+        ? num * 3
+        : num * 2;
 
     return (
-      <div style={{ height: `${height}px`, width: `${width}px` }} key={idx} className={`bar  ${isThisOneDone ? "thisOneInPlace" : ""} `}>{(length === 10 || length === 4) && `${num}`}</div>
+      <div
+        style={{ height: `${height}px`, width: `${width}px` }}
+        key={idx}
+        className={`bar  ${isThisOneDone ? "thisOneInPlace" : ""} `}
+      >
+        {(length === 10 || length === 6) && `${num}`}
+      </div>
     );
   };
 
-
   const generateNewArray = () => {
-    const newOne = generateArray(length)
-    setArrayToSort(newOne)
-    var sorted = [...newOne]
-    sortFun(sorted)
-    setSortedArray(sorted)
-  }
-
+    const newOne = generateArray(length);
+    setArrayToSort(newOne);
+    var sorted = [...newOne];
+    sortFun(sorted);
+    setSortedArray(sorted);
+    setLogs([
+      {
+        title: true,
+        body: `The array to sort is`,
+        array: JSON.stringify(newOne),
+      },
+    ]);
+  };
 
   useEffect(() => {
-    generateNewArray()
-  }, [length])
+    generateNewArray();
+  }, [length]);
 
-  const handleLength =  (e) => {
-    const newLength = parseInt(e.target.value) 
-    setLength(newLength)
-    if(newLength === 4) setSpeed(1000)
-    else if(newLength === 10) setSpeed(500)
-    else if (newLength === 50) setSpeed(100)
-    else if (newLength === 100) setSpeed(20)
-    else setSpeed(5)
-  }
-  
+  const handleLength = (e) => {
+    const newLength = parseInt(e.target.value);
+    setLength(newLength);
+    if (newLength === 4) setSpeed(1000);
+    else if (newLength === 10) setSpeed(300);
+    else if (newLength === 50) setSpeed(100);
+    else if (newLength === 100) setSpeed(20);
+    else setSpeed(5);
+  };
 
   return (
     <div className="insertion">
       <h1>Insertion sort</h1>
       <div className="options">
-        <button disabled={sorting === true} onClick={generateNewArray}>Generate new array</button>
-        <button onClick={sort} disabled={sorting === true}>Sort array</button>
-        <button disabled={sorting === true}>See Logs</button>
-        <select disabled={sorting === true} onChange={handleLength} >
-          <option value={4}>Array's length</option>
-          <option value={10} >10 elements</option>
-          <option value={50} >50 elements</option>
-          <option value={100} >100 elements</option>
-          <option value={150} >150 elements</option>
+        <button disabled={sorting === true} onClick={generateNewArray}>
+          Generate new array
+        </button>
+        <button onClick={sort} disabled={sorting === true}>
+          Sort array
+        </button>
+        <button disabled={sorting === true} onClick={() => setShlowLogs(true)}>
+          See Logs
+        </button>
+        <select disabled={sorting === true} onChange={handleLength}>
+          <option value={6}>Array's length</option>
+          <option value={10}>10 elements</option>
+          <option value={50}>50 elements</option>
+          <option value={100}>100 elements</option>
+          <option value={150}>150 elements</option>
         </select>
       </div>
       <div className="bars">
-        {
-          arrayToSort?.length > 0 && arrayToSort.map((ele, idx) => returnBarForNum(ele, idx))
-        }
+        {arrayToSort?.length > 0 &&
+          arrayToSort.map((ele, idx) => returnBarForNum(ele, idx))}
       </div>
+      {showLogs && (
+        <div className="logs">
+          <div className="logs_body">
+            <div className="logs__header">
+              <h1>LOGS</h1>
+              <button onClick={() => setShlowLogs(false)}>Go back</button>
+            </div>
+            <div className="logs__content">
+              {logs?.map((ele, idx) => {
+                return (
+                  <div key={idx}>
+                    {ele?.title ? (
+                      <h2>
+                        {ele.body} {ele.array}
+                      </h2>
+                    ) : (
+                      <>
+                        <p>{ele.body} {ele?.array && <b>{ele.array}</b>}</p>
+                        
+                      </>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
